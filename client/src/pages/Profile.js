0f32d6a2d987088c8a7caa5658163b95b58c6cfc
CoderWarrior48 <dawson@hamera.com>
2024-08-19 17:59:46 -0700

    added temp solution to admin dash redirect

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  
+
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
+  if (user.user_roles.includes("Admin")) {
+    return <Navigate to="/admin" replace={true}></Navigate>
+  }
   return (
     isAuthenticated && (
       <div>
