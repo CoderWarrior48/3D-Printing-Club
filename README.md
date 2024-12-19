73fafe7d1d8aa9567e9b73d08b41eb07d29565f1
Dawson Hamera <77896436+CoderWarrior48@users.noreply.github.com>
2024-12-19 04:38:30 +0000

    switched to native

--- README.md
+++
@@ -1,6 +0,0 @@
-# 3D-Printing-Club
-
-Server: Apache
-Frontend: React
-Backend: PHP
-Database: MySQL
--- client/src/pages/AdminDashboard.js
+++
@@ -1,9 +0,0 @@
-import Carousel from "../components/Carousel/Carousel";
-
-const AdminDashboard = () => {
-  return (
-    <p>If you're an admin, you alone have access to this page. The plan is to have this as a dashboard with all nessecary controls in one place.</p>
-  );
-};
-
-export default AdminDashboard;
