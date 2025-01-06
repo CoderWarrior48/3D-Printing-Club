6f53bf4b5c86982cab88ced1d056fa4b00c2094a
Dawson Hamera <77896436+CoderWarrior48@users.noreply.github.com>
2025-01-06 16:18:36 +0000

    gluestack

--- 3dpc/app/_layout.tsx
+++ 3dpc/app/_layout.tsx
@@ -1,26 +1,32 @@
 import { Stack } from 'expo-router';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import React from 'react';
 import { AuthProvider, useAuth } from './context/AuthContext';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-    
+    <GluestackUIProvider mode="light"><AuthProvider>
+        <App />
+      </AuthProvider></GluestackUIProvider>
   );
 }
 
 export const App = () => {
   const { authState, onLogout } = useAuth();
   return (
+    <>
+    <StatusBar style="light" />
+
     <Stack>
       {authState?.authenticated ? (        
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       ):(
-        <Stack.Screen name="index" options={{ title: 'Login' }} />
+        <Stack.Screen name="index" options={{ headerShown: false }}/>
       )}
         <Stack.Screen name="+not-found" />
     </Stack>
+    </>
   )
 }
--- 3dpc/app/index.tsx
+++ 3dpc/app/index.tsx
@@ -1,84 +1,36 @@
-import { View, StyleSheet, Text, Alert } from "react-native";
-import { router } from "expo-router";
-import { useState } from "react";
-import { useAuth } from "./context/AuthContext";
-import React from "react";
-import Button from "@/components/Button";
-import { Image } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Input, InputField } from '@/components/ui/input';
+import { Icon } from 'react-native-paper';
+import { Button, ButtonText } from '@/components/ui/button';
+import { Box } from '@/components/ui/box';
+import { VStack } from '@/components/ui/vstack';
 
 
-const PlaceholderImage = require("@/assets/images/3dpc.png");
+export default function Login() {
+  return (
+    <VStack justifyContent="space-between">
 
-
-export default function Login () {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { onLogin, onRegister } = useAuth();
-
-    const login = async () => {
-        const result = await onLogin!(email, password);
-        if (result && result.error) {
-            alert(result.msg);
-        }
-    };
-
-
-    const register = async () => {
-        const result = await onRegister!(email, password);
-        if (result && result.error) {
-            alert(result.msg);
-        }
-        else {
-            login();
-        }
-    }
-
-    return (
-        <View style={styles.container}>
-      <View style={styles.imageContainer}>
-      <Image source={PlaceholderImage} style={styles.image} />
-      </View>
-      <Text style={styles.header}>3DPC</Text>
-      <View style={styles.footerContainer}>
-        <Button label="Sign in" />
-        <Button
-          theme="primary"
-          label="Create an Account"
-          icon="user-plus"
-          onPress={login}
-        />
-        {!email ? (
-          <Text>You are not logged in</Text>
-        ) : (
-          <Text>You are logged in</Text>
-        )}
-      </View>
-    </View>
-    )
+      
+      <Input variant="outline" size="md" >
+          <InputField
+            placeholder='Username'
+          />
+        </Input>
+        <Input variant="outline" size="md">
+          <InputField
+            placeholder='Password'
+          />
+        </Input>
+        
+        <Button size="md" variant="solid" action="primary" >
+          <ButtonText>Hello World!</ButtonText>
+        </Button>
+      
+      
+    </VStack>
+  )
 }
-
-const styles = StyleSheet.create({
-    header: {
-      color: "#fff",
-      fontSize: 45,
-      fontWeight: 800,
-      margin: 30,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: "#25292e",
-      alignItems: "center",
-    },
-    imageContainer: {
-      flex: 0.8,
-    },
-    footerContainer: {
-      flex: 1 / 2,
-      alignItems: "center",
-    },
-    image: {
-        width: 350,
-        height: 440,
-        borderRadius: 18,
-      },
-  });
